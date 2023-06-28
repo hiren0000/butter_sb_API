b5f3aa7211e3572528ffa3e44d068a32efdff049
Hiren <rebeldevmurari@gmail.com>
2023-06-28 21:03:34 +0100

    First Commit

---
+++ src/main/java/com/rebel/Ecom/Butter_sb_API/ButterSbApiApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.Ecom.Butter_sb_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterSbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterSbApiApplication.class, args);
+	}
+
+}
